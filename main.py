03ae94c5e7e6b0bed4d1c5ed948380b88f11fcdc
Basangouda <basangoudams@gmail.com>
2023-01-21 13:20:44 -0800

    REV 2 - added tested API code

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import serial
 from time import sleep
+import requests
 
 
 
 ser = serial.Serial("/dev/ttyS0", 9600)
+ENDPOINT = "http://10.0.0.120:3000/name"
 
 while True:
     received_data = ser.read()
@@ -11,5 +13,35 @@ while True:
     data_left = ser.inWaiting()
     received_data += ser.read(data_left)
     print(received_data)
+    
+    received_data = received_data.decode()
+    
+    split_data = received_data.split('$')
+    
+    
+    
+    try:
+        gnrmc_string = split_data[4]
+    except IndexError:
+        continue
+    
+    gnrmc_list = gnrmc_string.split(",")
+    
+    if gnrmc_list[0] != "GNRMC":
+        continue
+
+
+    latitude = gnrmc_list[3]
+    longitude = gnrmc_list[5]
+
+
+    data = {"longitude":longitude,
+            "latitude":latitude}
+
+    send_data = requests.post(url=ENDPOINT, json=data)
+    
+    print(send_data.text)
+    
+    
     
     
